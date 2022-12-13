18b7ccb03c12a7c46bdbb0b16e0c3e415a1b260b
onahte <ethan.oh@gmail.com>
2022-12-13 13:37:17 -0500

    Edited db init

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -16,9 +16,10 @@ database = Blueprint('database', __name__, )
 URL = config.Config.SQLALCHEMY_DATABASE_URI
 engine = create_engine(URL)
 
-'''
+
 def init_db():
-    '''try:
+    pass
+'''try:
         connection = psycopg2.connect(URL)
         print("ElephantSQL connection successful")
     except:
@@ -28,33 +29,32 @@ def init_db():
         print("Creating database...")
         create_database(URL)'''
 
-    with engine.connect() as connection:
-        db.drop_all()
-        db.create_all()
+#    with engine.connect() as connection:
+#        db.drop_all()
+#        db.create_all()
 
-    '''This is a list of all our tables. 
-        Order the list by putting tables w/ no Foreign Keys first'''
-    tables = ['address', 'allergy', 'physician', 'bed', 'nurse',
-              'patient', 'clinic', 'consultation', 'contract', 'corporate',
-              'gender', 'illness', 'inpatient', 'medical_data', 'medication',
-              'nurse_assign_surgery', 'nurse_assign_inpatient', 'nurse_schedule',
-              'own', 'skill_possessed', 'prescription', 'salary', 'skill',
-              'surgery_schedule', 'staff', 'surgeon', 'surgery',
-              'work', 'physician_schedule', 'staff_schedule', 'nurse_unit']
-    for table in tables:
-        populate_table(table)
-    ''' 
+'''This is a list of all our tables. 
+        Order the list by putting tables w/ no Foreign Keys first'''#
+#    tables = ['address', 'allergy', 'physician', 'bed', 'nurse',
+#              'patient', 'clinic', 'consultation', 'contract', 'corporate',
+#              'gender', 'illness', 'inpatient', 'medical_data', 'medication',
+#              'nurse_assign_surgery', 'nurse_assign_inpatient', 'nurse_schedule',
+#              'own', 'skill_possessed', 'prescription', 'salary', 'skill',
+#              'surgery_schedule', 'staff', 'surgeon', 'surgery',
+#              'work', 'physician_schedule', 'staff_schedule', 'nurse_unit']
+#    for table in tables:
+#        populate_table(table)
+''' 
     Foreign Key error occurs when implementing:
     for filename in os.listdir(current_app.config['CSV_DIR']):
         basename, extension = os.path.splitext(filename)
-    '''
-    connection.close()
-    engine.dispose()
-
-
-def populate_table(table):
-    filename = table + '.csv'
-    filepath = os.path.join(current_app.config['CSV_DIR'], filename)
-    data = pd.read_csv(filepath)
-    data.to_sql(table, engine, if_exists='append', index=False)
 '''
+#    connection.close()
+#    engine.dispose()
+
+
+#def populate_table(table):
+#    filename = table + '.csv'
+#    filepath = os.path.join(current_app.config['CSV_DIR'], filename)
+#    data = pd.read_csv(filepath)
+#    data.to_sql(table, engine, if_exists='append', index=False)
